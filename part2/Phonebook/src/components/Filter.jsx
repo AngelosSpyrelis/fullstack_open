e928ddc4b46dee4254b04b344df56be0d744b8d6
AngelosSpyrelis <angel@aceitdesign.com>
2026-07-22 12:33:46 +0300

    Add files via upload

--- part2/Phonebook/src/components/Filter.jsx
+++ part2/Phonebook/src/components/Filter.jsx
@@ -1,3 +1,3 @@
 export const Filter = ({filter, callback}) =>{
-    return(<label>Filter:<input type='text' value={filter} data-state="filter" onChange={callback} /></label>);
+    return(<label>Filter:<input type='text' value={filter} name="filter" onChange={callback} /></label>);
 }
