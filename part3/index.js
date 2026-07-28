a5cc91295026ddd17dc7c510ad0f6c4587b89638
AngelosSpyrelis <angel@aceitdesign.com>
2026-07-28 09:53:22 +0300

    Add files via upload

--- part3/index.js
+++ part3/index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-const datasets = require('./datasets/persons');
+const Person = require('./models/database');
+
 
 const app = express();
-
 
 const logger = (tokens, req, res) =>{
     
@@ -30,62 +31,86 @@ app.use(express.static('dist'));
 
 
 
-app.get('/api/persons', (request, response) => {
-    response.json(datasets.persons);
+app.get('/api/persons', (request, response, next) => {
+    Person.find({}).then(result => {
+        return response.json(result)}).catch(error=>next(error));
 });
 
-app.get('/api/info', (request, response) =>{
-    const number = datasets.persons.length;
-    const date = new Date().toString();
-    response.send(`Phonebook has infor for ${number} persons.<br />${date}`);
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id).then(result=>{
+        if(!result){
+            response.status(404).end();
+        }
+        else{
+            response.json(result);
+        }
+    }).catch(error=>next(error));
 });
 
-app.get('/api/persons/:id', (request, response) =>{
-    const id = request.params.id;
-    const found = datasets.persons.filter(person=>person.id == id);
-    if(found.length == 0){
-        response.status(404).end("There is no person with the requested id.");
-    }
-    else{
-        response.json(found);
-    }
+app.get('/api/info', (request, response, next) =>{
+    Person.find({}).then(result => {
+        const number = result.length;
+        const date = new Date().toString();
+        response.send(`Phonebook has infor for ${number} persons.<br />${date}`);
+    }).catch(error=>next(error));
     
 });
 
-app.delete('/api/persons/:id', (request, response)=>{
-    const id = request.params.id;
-    const initLength = datasets.persons.length;
-    datasets.persons = datasets.persons.filter(person=>person.id != id);
-    if(initLength != datasets.persons.length){
-        response.status(204).end();
-    }
-    else{
-        response.status(404).end("There is no person with the requested id.");
-    }
-
+app.delete('/api/persons/:id', (request, response, next)=>{
+    Person.findByIdAndDelete(request.params.id).then(result=>{
+        if(!result){
+            response.status(404).end();
+        }
+        else{
+            response.status(204).end();
+        }   
+        
+    }).catch(error=>next(error));
 });
 
-app.post('/api/persons', (request, response)=>{
-    const minValue = 0;
-    const maxValue = minValue+1000;
-    const id = (Math.floor(Math.random()*(maxValue-minValue+1))+minValue).toString();
+app.post('/api/persons', (request, response, next)=>{
+
+    const person = new Person({
+        name: request.body.name,
+        number: request.body.number
+    });
     
-    const newPerson = {...request.body,id:id};
-    const nameRegex = new RegExp(`^${newPerson.name}$`, 'i');
-    if(!newPerson.name || !newPerson.number){
-        response.status(400);
-        response.json({error: "New entries must have a name and a number."});
-    }
-    else if(datasets.persons.filter(person=>newPerson.name === person.name).length > 0){
-        response.status(400);
-        response.json({error: `There is already an entry for ${newPerson.name} in the phonebook.`});
-    }
-    else{
-        datasets.persons.push(newPerson);
-        response.status(200).end();
-    }
+    person.save().then(result => response.json(resut)).catch(error=>next(error));
+
     
 });
 
-const PORT = process.env.PORT || 3001
+app.put('/api/persons/:id', (request, response, next)=>{
+    
+    Person.findById(request.params.id).then(person=>{
+        if(person){
+            person.number = request.body.number;
+            
+            person.save().then(result => response.json(result)).catch(error=>next(error));
+        }
+        else{
+            response.status(404).end();
+        }
+    }).catch(error=>next(error));
+});
+
+const errorHandler = (error, request, response, next) => {
+    
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    else{
+        console.log(error.message);
+        return response.status(400).send({ error: `${error.name} ${error.message}`});
+    } 
+
+    next(error);
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {});
