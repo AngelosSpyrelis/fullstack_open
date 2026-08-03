100927689f0d1e9ae01d954a6f98c210a8a8300c
AngelosSpyrelis <angel@aceitdesign.com>
2026-08-03 13:04:45 +0300

    Add files via upload

---
+++ part4/app.js
@@ -0,0 +1,27 @@
+const config = require('./utils/config');
+const middleware = require('./utils/middleware');
+const express = require('express');
+const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
+const mongoose = require('mongoose');
+const url = config.MONGODB_URI;
+const app = express();
+
+
+mongoose.set('strictQuery',false);
+mongoose.connect(url, { family: 4 });
+
+
+app.use(express.json());
+app.use(middleware.tokenExtractor);
+app.use(express.static('dist'));
+app.use(middleware.requestLogger);
+
+app.use('/api/users', usersRouter);
+app.use('/api/blogs', middleware.userExtractor, blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+
+module.exports = app;
---
+++ part4/controllers/blogs.js
@@ -0,0 +1,49 @@
+const blogsRouter = require('express').Router();
+const Blog = require('../models/blogs');
+
+
+
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({}).populate('user');
+    return response.json({ success: true, data: blogs });
+});
+
+blogsRouter.get('/:id', async (request, response) => {
+    const blog = await Blog.findById(request.params.id).populate('user');
+    return response.status(200).json({ success: true, data: blog });
+});
+
+blogsRouter.post('/', async (request, response) => {
+
+    const blog = new Blog(request.body);
+
+    const result = await blog.save();
+    const populatedResult = await result.populate('user');
+    return response.status(201).json({ success: true, data: populatedResult });;
+
+
+});
+
+blogsRouter.delete('/', async (request, response, next) => {
+
+    const blog = await Blog.findById(request.body.id);
+    if(blog.user.toString() !== request.body.user._id.toString()){
+        return next({ name:'AuthenticationError', message: 'Blogs cannot be deleted without a valid token.' });
+    };
+
+    await Blog.findByIdAndDelete(request.body.id);
+    return response.status(204).end();
+});
+
+blogsRouter.put('/:id', async (request, response) => {
+
+    const blog = await Blog.findById(request.params.id);
+    blog.likes = request.body.likes;
+
+    const result = await blog.save();
+    return response.status(201).json({ success: true, data: result });
+});
+
+
+
+module.exports = blogsRouter;
